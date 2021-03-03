dcecee314f993aaadcbc73242caf87cab9cb1944
fredeckri <73676607+fredeckri@users.noreply.github.com>
2021-03-02 23:48:13 -0300

    Update Data.js

--- Data.js
+++ Data.js
@@ -141,5 +141,11 @@ class Data {
 
     }
 
-
+   dataInvertida(){
+    var data = new Date(),
+        dia  = data.getDate().toString().padStart(2, '0'),
+        mes  = (data.getMonth()+1).toString().padStart(2, '0'), //+1 pois no getMonth Janeiro começa com zero.
+        ano  = data.getFullYear();
+    return ano+"-"+mes+"-"+dia;
+}
 }
